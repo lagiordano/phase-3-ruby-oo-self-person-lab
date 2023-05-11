3236b10baf22c3802ef91811bf979ad352b0eba5
Lauren Giordano <laurengiordano94@gmail.com>
2023-05-11 13:52:43 +1000

    commit tree dbb4b222175a179adab71db81eb55284ef11f1e7

--- lib/person.rb
+++ lib/person.rb
@@ -1,80 +1,97 @@
-# your code goes here
-
 class Person 
-    attr_reader :name, :happiness, :hygiene
     attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
 
-    def initialize name, bank_account=25
-        @name = name 
-        @bank_account = bank_account
+    def initialize name
+        @name = name
+        @bank_account = 25
         @happiness = 8
         @hygiene = 8
     end
 
     def happiness=(happiness)
-        if happiness < 0
-            @happiness = 0
-        elsif happiness > 10 
+        if happiness > 10
             @happiness = 10
+        elsif happiness < 0 
+            @happiness = 0
         else
             @happiness = happiness
         end
     end
 
     def hygiene=(hygiene)
-        if hygiene < 0
-            @hygiene = 0
-        elsif hygiene > 10 
+        if hygiene > 10 
             @hygiene = 10
+        elsif hygiene < 0 
+            @hygiene = 0
         else
             @hygiene = hygiene
         end
     end
 
     def happy? 
-        self.happiness > 7 ? true : false
+        self.happiness > 7
     end
 
     def clean? 
-        self.hygiene > 7 ? true : false
+        self.hygiene > 7
     end
 
-    def get_paid(amount)
-        self.bank_account += amount 
+    def get_paid amount
+        self.bank_account = self.bank_account + amount
         "all about the benjamins"
     end
 
-    def take_bath 
-        self.hygiene += 4 
+    def take_bath
+        self.hygiene = self.hygiene + 4
         "♪ Rub-a-dub just relaxing in the tub ♫"
     end
 
     def work_out
-        self.happiness += 2
-        self.hygiene -= 3 
+        self.hygiene = self.hygiene - 3
+        self.happiness = self.happiness + 2
         "♪ another one bites the dust ♫"
     end
 
-    def call_friend(friend)
+    def call_friend friend
         self.happiness += 3
         friend.happiness += 3
         "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
-    def start_conversation(friend, topic)
-        if topic == "politics"
-            self.happiness -= 2
-            friend.happiness -= 2
-            "blah blah partisan blah lobbyist"
-        elsif topic == "weather"
-            self.happiness += 1
-            friend.happiness += 1
-            "blah blah sun blah rain"
-        else 
-            "blah blah blah blah blah"
-        end
+    # def start_conversation person, topic 
+    #     if topic == "politics"
+    #         self.happiness -= 2
+    #         person.happiness -= 2
+    #         'blah blah partisan blah lobbyist'
+    #     elsif topic == "weather"
+    #         self.happiness += 1
+    #         person.happiness += 1
+    #         'blah blah sun blah rain'
+    #     else 
+    #         'blah blah blah blah blah'
+    #     end
+    # end
+            
+ def start_conversation person, topic
+    case topic 
+    when 'politics'
+        self.happiness -= 2
+        person.happiness -= 2
+        'blah blah partisan blah lobbyist'
+    when 'weather'
+        self.happiness += 1
+        person.happiness += 1
+        'blah blah sun blah rain'
+    else
+        'blah blah blah blah blah'
     end
+end
+
+
+
 
 
 
 end
+
